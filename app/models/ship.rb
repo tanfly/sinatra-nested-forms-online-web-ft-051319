a0d1660900cd4f79879d21a94805a9816077f958
tanfly <mcewen.stella@gmail.com>
2019-06-27 13:52:20 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
-end
+
+    @@ships = []
+
+    attr_accessor :name, :type, :booty
+  
+     def initialize(params)
+        @name = params[:name] 
+        @type = params[:type]
+        @booty = params[:booty]
+        @@ships << self 
+    end 
+  
+     def self.all 
+        @@ships 
+    end 
+  
+     def self.clear 
+        @@ships.clear 
+    end 
+  end 
